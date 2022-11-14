5efd62f3c333a38093ac3b1d5137b2255ab8f9ed
DESKTOP-KMJA3JG\takut <20217024-kawaitakuto@hcs.ac.jp>
2022-11-14 15:03:18 +0900

    メインシーンとマウス操作の追加

---
+++ Assets/script/Shoot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    [SerializeField]
+    GameObject Camera;//メインのカメラ
+    RaycastHit hit;//当たった判定を格納する
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(Camera.transform.position, Camera.transform.forward, out hit, 1000f))//画面の中心からrayを飛ばす
+            {
+                if (hit.collider.tag == "")
+                {
+                    hit.collider.gameObject.SetActive(false);
+                }
+
+
+            }
+        }
+        
+    }
+}
