bc86aaa6634a116ca7708e4d9741338023347ee0
DESKTOP-KMJA3JG\takut <20217024-kawaitakuto@hcs.ac.jp>
2022-11-15 15:02:00 +0900

    レイキャストでの敵を倒すスクリプトの追加

--- Assets/script/Shoot.cs
+++ Assets/script/Shoot.cs
@@ -9,18 +9,26 @@ public class Shoot : MonoBehaviour
     RaycastHit hit;//当たった判定を格納する
     private void Update()
     {
+        Debug.DrawRay(transform.position, transform.forward * 10, Color.red);
         if (Input.GetMouseButtonDown(0))
         {
+            
             if (Physics.Raycast(Camera.transform.position, Camera.transform.forward, out hit, 1000f))//画面の中心からrayを飛ばす
             {
-                if (hit.collider.tag == "")
+                
+                if (hit.collider.tag == "Enemy")
                 {
                     hit.collider.gameObject.SetActive(false);
+                    StartCoroutine("CubeAppear");
                 }
-
 
             }
         }
         
+    }
+    IEnumerator CubeAppear()
+    {
+        yield return new WaitForSeconds(2.0f);
+        hit.collider.gameObject.SetActive(true);
     }
 }
